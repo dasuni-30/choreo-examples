d92610580d94151a1db59fd5574b8b9d7b63f096
Dasuni Geeganage <dasunin30@gmail.com>
2024-04-18 13:42:35 +0530

    Add configs file

--- b2c-apps/pet-care-app/pet-management-webapp/config.js
+++ b2c-apps/pet-care-app/pet-management-webapp/config.js
@@ -1,7 +1,12 @@
 window.config = {
-    baseUrl: "",
-    clientID: "",
-    signInRedirectURL: "",
-    signOutRedirectURL: "",
-    resourceServerURL: ""
+    baseUrl: "https://api.asgardeo.io/t/dasuorg",
+    clientID: "DhBRGQmNg3BvL7kD0zCb237V92Ia",
+    scope: [
+        "openid",
+        "profile",
+        "email"
+    ],
+    signInRedirectURL: "https://df86c6a7-edb2-4194-abcd-1cecec180eec.e1-us-east-azure.choreoapps.dev",
+    signOutRedirectURL: "https://df86c6a7-edb2-4194-abcd-1cecec180eec.e1-us-east-azure.choreoapps.dev",
+    resourceServerURL: "https://71fe9995-65a1-4e05-92a8-bc40749649d8-prod.e1-us-east-azure.choreoapis.dev/hmvi/pet-management-service/pet-management-service-803/1.0.0"
 };
